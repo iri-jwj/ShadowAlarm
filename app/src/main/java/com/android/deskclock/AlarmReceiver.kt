c435dc512f2b84fef7a18e1ec9450f6d55d110ff
iri.jwj <iri.jwj@outlook.com>
2019-08-26 20:59:43 +0800

    完成广播部分除 delay 以外的逻辑

--- app/src/main/java/com/android/deskclock/AlarmReceiver.kt
+++ app/src/main/java/com/android/deskclock/AlarmReceiver.kt
@@ -1,12 +1,56 @@
 package com.android.deskclock
 
+import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.PowerManager
+import com.android.deskclock.util.AlarmManagerUtil
+import com.android.deskclock.util.AlarmNotificationUtil
 
 class AlarmReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-
+    companion object {
+        const val ACTION_DELAY = "com.android.deskclock.actionDelay"
     }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent!!.action) {
+            ACTION_DELAY -> {
+
+            }
+            AlarmManagerUtil.alarmAction -> {
+                handleNormalAlarmAction(context, intent)
+            }
+        }
+    }
+
+    private fun handleNormalAlarmAction(context: Context?, intent: Intent) {
+        val powerManager = context?.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val isScreenLighted = powerManager.isInteractive
+        val keyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        val isLocked = keyguardManager.isKeyguardLocked
+
+        val label = intent.getStringExtra("label")
+        val id = intent.getIntExtra("id", 0)
+        if (isScreenLighted and !isLocked) {
+            showAlarmByNotification(context, label, id)
+        } else {
+            showAlarmByOverlayWindow(context, label, id)
+        }
+    }
+
+
+    private fun showAlarmByOverlayWindow(context: Context, label: String?, id: Int?) {
+        val intent = Intent(context, AlarmOverlayService::class.java)
+        intent.putExtra("label", label)
+        intent.putExtra("id", id)
+        context.startService(intent)
+    }
+
+    private fun showAlarmByNotification(context: Context, label: String?, id: Int?) {
+        AlarmNotificationUtil(context, label!!, id!!).showNotification()
+    }
+
+
 }
---
+++ app/src/main/java/com/android/deskclock/util/AlarmNotificationUtil.kt
@@ -0,0 +1,60 @@
+package com.android.deskclock.util
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import com.android.deskclock.R
+import com.android.deskclock.homepage.MainActivity
+
+class AlarmNotificationUtil(
+    private val context: Context,
+    private val label: String,
+    private val id: Int
+) {
+    private val channelId = "com.android.deskclock.notification"
+    private val channelName = "ShadowAlarm's normal notification"
+    private lateinit var channel: NotificationChannel
+    private val notificationManager: NotificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    private var builder: Notification.Builder
+
+    init {
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            channel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            builder = Notification.Builder(context, channelId)
+            notificationManager.createNotificationChannel(channel)
+        } else {
+            builder = Notification.Builder(context)
+        }
+
+
+    }
+
+    fun showNotification() {
+        val remoteView = RemoteViews(context.packageName, R.layout.notifcaion_alarm)
+
+        remoteView.setTextViewText(R.id.notification_label, label)
+
+        val intent = Intent(context, MainActivity::class.java)
+
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        remoteView.setOnClickPendingIntent(R.id.notification_alarm_parent, pendingIntent)
+
+        val notification = builder.setCustomContentView(remoteView)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setFullScreenIntent(pendingIntent, true).build()
+
+        notificationManager.notify(id, notification)
+    }
+
+
+}
