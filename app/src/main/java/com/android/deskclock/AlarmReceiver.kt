3095929fde6548bb69fc071e72118ed3bd3e135a
jianweijie <11104705@bbktel.com>
2019-08-26 17:25:15 +0800

    广播与服务开始

---
+++ app/src/main/java/com/android/deskclock/AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package com.android.deskclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
